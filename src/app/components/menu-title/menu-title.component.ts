30ba49526e1aa752ccc357f7d0b86c2f50279cd3
thaissa <thaissaduraes67@gmail.com>
2024-02-24 21:36:20 -0300

    component de style menu-title created

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.scss']
+  styleUrls: [
+    './menu-title.component.scss',
+    './menu-title.responsive.component.scss',
+  ],
 })
-export class MenuTitleComponent {
-
-}
+export class MenuTitleComponent {}
